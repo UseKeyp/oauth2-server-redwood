bd83a9cfbb334fd903f0be910b8d042c5481c48c
pi0neerpat <blockchainbuddha@gmail.com>
2023-01-24 13:54:09 -0800

    Handle no access token provided

--- api/src/functions/v1/api/index.js
+++ api/src/functions/v1/api/index.js
@@ -20,25 +20,30 @@ const app = (db, settings) => {
   // }
 
   const validateToken = async (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1]
-    const response = await fetch(
-      settings.APP_DOMAIN + '/oauth/token/introspection',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        },
-        body: encodeBody({
-          token,
-          client_id: 'api-server',
-          client_secret: process.env.INTROSPECTION_SECRET,
-        }),
-      }
-    ).then((res) => res.json())
-    console.log(response)
-    if (!response.active) return res.status(401).send('Unauthorized')
-    req.ctx = { user: response.sub, scope: response.scope }
-    next()
+    try {
+      const token = req?.headers?.authorization?.split(' ')[1]
+      const response = await fetch(
+        settings.APP_DOMAIN + '/oauth/token/introspection',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          },
+          body: encodeBody({
+            token,
+            client_id: 'api-server',
+            client_secret: process.env.INTROSPECTION_SECRET,
+          }),
+        }
+      ).then((res) => res.json())
+      console.log(response)
+      if (!response.active) return res.status(401).send('Unauthorized')
+      req.ctx = { user: response.sub, scope: response.scope }
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Internal Server Error')
+    }
   }
 
   // Express docs https://expressjs.com/en/5x/api.html#app.settings.table
